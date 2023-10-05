8608bee9f9a9af41434b839f2023b8c927678df4
Deniel <kankan8@bk.ru>
2023-10-05 17:43:58 +0200

    Commit message for test

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <conio.h>
 #include <windows.h>	// Include windows.h library for Sleep() function
 
-//#define debug
+#define debug
 
 #ifdef debug							// define "debug" while the programm is beeing testet so assert-functions get compiled 
 #include <assert.h>
@@ -35,11 +35,11 @@ void showTargetedOpt(int opt)
 	puts("Calculate acceleration");
 }
 
-void rangeOfOption(int* opt)
+void rangeOfOption(int* opt, int range1, int range2)
 {
-	if (*opt < 0)			
+	if (*opt < range1)			
 		*opt = 0;			
-	else if (*opt > 1)		
+	else if (*opt > range2)		
 		*opt = 1;			
 }
 
@@ -50,7 +50,7 @@ void moveTheArrow(char key1, char key2, int* opt)
 	else if (key1 == -32 && key2 == 80)
 		(*opt)++;
 
-	rangeOfOption(opt);		//	Limits the option code so it's not out of range
+	rangeOfOption(opt, 0, 1);		//	Limits the option code so it's not out of range
 }
 
 char chooseOption()
@@ -61,18 +61,16 @@ char chooseOption()
 	while (1)
 	{
 		puts("Choose the option\t\t\tArrow up - Up\tArrow down - Down\tEnter - Choose\n\n\n\n");
-
 		
 		showTargetedOpt(opt);		// Shows the targeted option by arrow
 
-
-		key1 = _getch();	// reading the 2 keys because  arrows give 2 values - Here: The first key
+		key1 = _getch();	// reading 2 keys because arrows give 2 values - Here: The first key
 		system("cls");		
 		if (key1 == 13)		// If key is Enter (with ASCII code 13), the function returns the 
 			return opt;
+		else if (key1 != -32)
+			continue;
 		key2 = _getch();	// Here: the second key
-
-		
 
 		moveTheArrow(key1, key2, &opt);	// Moves the arrow depending on key by changing the option
 	}
@@ -82,7 +80,7 @@ char chooseOption()
 
 double calcV()
 {
-	double s = 0, v = 0, t = 0;
+	double s = 0, t = 0;
 
 	puts("s - The lastet way in m\t v - The speed in m/s\t t - The time in s");
 	puts("    s");
@@ -93,19 +91,59 @@ double calcV()
 	scanf("%lf", &s);
 	puts("Type t in s:");
 	scanf("%lf", &t);
+	puts("");
 
-	double res = s / t;
-	printf("Result: v = %.7lf m/s\n\n", res);
+	double v = s / t;
+	
+	int opt = 0;
 
-	puts("Press any key to continue.");
-	char any_key = _getch();
+	while (1)																				// Offering the option to calculate the acceleration when needed
+	{																						// Constructed similar to the function char chooseOption()
+		printf("Result: v = %.7lf m/s\n\n", v);
+		puts("Use Up/Down button to switch\t Use the Enter button to choose\n\n\n");
+
+		if (opt == 0)
+			printf("--> ");
+		else
+			printf("    ");
+		puts("Calculate acceleration \"a\" with the known velocity \"v\" and return to menu after it");
+
+		if (opt == 1)
+			printf("--> ");
+		else
+			printf("    ");
+		puts("Return to menu");
+
+		char key1 = _getch();
+		system("cls");
+		if (key1 == 13)
+		{
+			if (opt == 0)
+			{
+				printf("Result: a = %lf m/s^2\n\n\n", v / t);
+
+				puts("Press any Button to continue");
+				char anyKey = _getch();
+				break;				// Leaving the cicle to clear the screen and leave the function
+			}
+			else if (opt == 1)
+				break;
+		}
+		else if (key1 != -32)
+			continue;
+		char key2 = _getch();
+
+		moveTheArrow(key1, key2, &opt);
+
+		puts("\n\n\n\n\n\n\n");
+	}
+
 	system("cls");
-
 }
 
 double calcA()
 {
-	double v = 0, a = 0, t = 0;
+	double v = 0, t = 0;
 
 	puts("a - The acceleration way in m/s^2\t v - The speed in m/s\t t - The time in s");
 	puts("    v");
@@ -116,14 +154,13 @@ double calcA()
 	scanf("%lf", &v);
 	puts("Type t in s:");
 	scanf("%lf", &t);
-
-	double res = v / t;
-	printf("Result: v = %.7lf m/s^2\n\n", res);
+	
+	double a = v / t;
+	printf("Result: v = %.7lf m/s^2\n\n", a);
 
 	puts("Press any key to continue.");
 	char any_key = _getch();
 	system("cls");
-
 }
 
 double redirectToChoice(int opt)
@@ -134,12 +171,22 @@ double redirectToChoice(int opt)
 			calcV();		// Calc. veloctity and prints the result.
 			break; 
 		case 1:
-			calcA();		// Calc. acceleration and prints the result.
+			calcA(0, 0);		// Calc. acceleration and prints the result.
 			break;
 	}
 }
 
+#ifndef debug
+void printWelcomeMessage()
+{
+	int greetingTimeout = 4000;		// The period of time in miliseconds where the welcome message is shown
 
+	puts("Calculator for physics 2023\n");																				// 
+	puts("Calculation of velocity and acceleration\nProgrammed on C\nPlease wait and don't press any button...");		//	 Welcome 
+	Sleep(greetingTimeout);																								//	 message
+	system("cls");																										//
+}
+#endif
 
 int main()
 {
@@ -147,13 +194,11 @@ int main()
 	testCase();			// starts the test operation
 	#endif
 
-	int option = 0, greetingTimeout = 4000;		// The period of time in miliseconds where the welcome message is shown
+	int option = 0;		
 
-	puts("Calculator for physics 2023\n");																				// 
-	puts("Calculation of velocity and acceleration\nProgrammed on C\nPlease wait and don't press any button...");		//	 Welcome 
-	Sleep(greetingTimeout);																								//	 message
-	system("cls");																										//
-	 
+	#ifndef debug
+	printWelcomeMessage();																							// PrintsTheWelcome Message
+	#endif 
 
 	while (1)
 	{
@@ -167,27 +212,27 @@ int main()
 void testRangeOfOption(int* opt)
 {
 	*opt = 0;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 0);
 
 	*opt = 1;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 1);
 
 	*opt = 2;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 1);
 
 	*opt = 3;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 1);
 
 	*opt = -1;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 0);
 
 	*opt = -2;
-	rangeOfOption(opt);
+	rangeOfOption(opt, 0, 1);
 	assert(*opt == 0);
 }
 
